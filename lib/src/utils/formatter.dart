28bdffef5c5b6cebcd4dc1f5a31b2b5fd8ba2d30
Kasia Zawistowska <zawistowska.kasia@outlook.com>
2025-04-23 11:16:08 +0100

    feat(`formatter.dart`) formatter now handles excluded columns as columns rather than string values
    TODO: might have to revisit for column use in general

--- lib/src/utils/formatter.dart
+++ lib/src/utils/formatter.dart
@@ -1,17 +1,19 @@
 import '../models/enums/enums.dart' show Condition;
 
 String formatValue(dynamic value, [bool between = false]) {
-    if (between) return "${value[0]} AND ${value[1]}";
+  if (between) return "${value[0]} AND ${value[1]}";
 
-    if (value is String) {
-      return "'$value'";
-    } else if (value is List<List>) {
-      return value.map((v) => formatValue(v)).join(', ');
-    } else if (value is List) {
-      return "(${value.map((v) => formatValue(v)).join(', ')})";
-    } else if (value is Condition) {
-      return "";
-    } else {
-      return value.toString();
-    }
+  if (value is String) {
+    final regex = RegExp(r'^excluded\.(?:[a-zA-Z_][a-zA-Z0-9_]*|"[^"]+")$');
+    if (regex.hasMatch(value)) return value;
+    return "'$value'";
+  } else if (value is List<List>) {
+    return value.map((v) => formatValue(v)).join(', ');
+  } else if (value is List) {
+    return "(${value.map((v) => formatValue(v)).join(', ')})";
+  } else if (value is Condition) {
+    return "";
+  } else {
+    return value.toString();
   }
+}
