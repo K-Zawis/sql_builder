cee15a4d7d5787a1f98491269814f2f07dff5ffe
Kasia Zawistowska <zawistowska.kasia@outlook.com>
2025-05-02 12:36:43 +0100

    chore(*) fixed pubspec.yaml

--- lib/src/models/builders/upsert_builder.module.dart
+++ lib/src/models/builders/upsert_builder.module.dart
@@ -1,24 +1,6 @@
 import '../../interfaces/interfaces.dart';
 import '../../utils/utils.dart';
 
-import 'update_builder.module.dart';
-
-// class UpsertQueryBuilder extends UpdateQueryBuilder {
-//   final QueryBuilder previous;
-//   final List<String> conflictColumns;
-//   UpsertQueryBuilder(this.previous, super.table, super.row, {required this.conflictColumns});
-
-//   @override
-//   String build([bool finalBuild = true]) {
-//     final String updateSetClause =
-//         row.keys.where((col) => !conflictColumns.contains(col))
-//         .map((col) => "$col=excluded.$col")
-//         .join(", ");
-
-//     return "${previous.build(false)} ON CONFLICT (${conflictColumns.join(", ")}) DO UPDATE SET $updateSetClause".withSemicolon(finalBuild);
-//   }
-// }
-
 class UpsertQueryBuilder extends QueryBuilder {
   final String table;
   final Map<String, dynamic> row;
